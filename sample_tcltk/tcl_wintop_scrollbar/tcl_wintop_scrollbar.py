ac4658a3cc743d87f70be8f152717d5dcc1f0ef7
sakurataiko1 <49112677+sakurataiko1@users.noreply.github.com>
2024-05-02 17:21:04 +0900

    Add files via upload

--- sample_tcltk/tcl_wintop_scrollbar/tcl_wintop_scrollbar.py
+++ sample_tcltk/tcl_wintop_scrollbar/tcl_wintop_scrollbar.py
@@ -1,3 +1,7 @@
+#
+# ウィンドウ全体にフレーム配置する
+#　　　垂直方向のスクロールバーがユーザー操作で動かされた時に処理実行する(printでメッセージ表示）
+
 import os
 
 import tkinter as tk
@@ -9,7 +13,7 @@ class MainWindow(tk.Frame):
         super().__init__(master)
 
         self.master.title("sample wintop scrollbar")
-        #self.master.geometry("2000x1500")  # (1300x600)  必ず、全部の部品が見えるサイズ以上にする
+        #self.master.geometry("2000x1500")  # (1300x600)  必ず、全部の部品が見えるサイズ以上にする. スクロールバーもこの範囲までが有効になる
         self.g_winsizeW = 1000
         self.g_winsizeH = 2000
         str_sizeWH = str(self.g_winsizeW) + "x" + str(self.g_winsizeH)
@@ -23,8 +27,9 @@ class MainWindow(tk.Frame):
         )
         # スクロールの設定
         self.canvas_wintop.configure(scrollregion=(0, 0, self.g_winsizeW, self.g_winsizeH))
-        self.canvas_wintop.configure(yscrollcommand=self.scrollbarV.set)
-
+        #self.canvas_wintop.configure(yscrollcommand=self.scrollbarV.set)
+        self.canvas_wintop.configure(yscrollcommand=self.func_on_vertical_scroll)
+        #
         # 水平方向スクロールバー
         self.scrollbarH = tk.Scrollbar(
             self.canvas_wintop, orient=tk.HORIZONTAL, command=self.canvas_wintop.xview
@@ -44,23 +49,44 @@ class MainWindow(tk.Frame):
         self.frame00 = tk.Frame(self.frame_wintop, relief='groove', bd=2)  # relief=枠線スタイル bd=枠線太さ
         self.frame01 = tk.Frame(self.frame_wintop, relief='groove', bd=2)
         # 配置　フレーム
-        self.frame00.grid(row=0, column=0, padx=5, pady=5, sticky="nsew") # DEBUGメニュー
-        self.frame01.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")  # プロット画面と上下メニュー
+        self.frame00.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        self.frame01.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
         # グリッドの設定  全体ウィンドウの伸縮に、各部品が追従するにはこれが必須
         for i in range(3):
             #root.rowconfigure(i, weight=1)
             root.columnconfigure(i, weight=1)
         root.rowconfigure(1, weight=1) # 画面リサイズ時の、縦方向の部品リサイズ追従は 1のみ(canvasを含むもの)
 
+        # 部品定義　0段目
+        self.canvas1 = tk.Canvas(self.frame00)
+        # 部品配置　0段目
+        self.canvas1.pack(side="left", fill="both", expand=True, padx=5)
+
         # 部品定義　1段目
-        self.button1 = ttk.Button(self.frame00, text="テスト")
+        self.button1 = ttk.Button(self.frame01, text="テスト", command=self.func_on_button1_cliecked)
+        self.GUIvalue_slider1 = tk.DoubleVar()
         # 部品配置　1段目
         self.button1.pack(side='left')
 
-        # 部品定義　2段目
-        self.canvas1 = tk.Canvas(self.frame01)
-        # 部品配置　2段目
-        self.canvas1.pack(side="left", fill="both", expand=True, padx=5)
+        # ユーザーに見えやすくするための処理。　全体サイズでは大きすぎるので、起動時は小さいWindowで起動する
+        #self.master.geometry("300x300")
+        tmp_w = 200
+        tmp_h = 100
+        str_sizeWH = str(tmp_w) + "x" + str(tmp_h)
+        self.master.geometry(str_sizeWH)
+
+    def func_on_vertical_scroll(self, *args):
+        # スクロールバーが動いた時の処理
+        #self.canvas_wintop.yview
+        self.scrollbarV.set(*args)
+        if self.scrollbarV.get()[1] == 0.0:
+            print("move_end")
+        else:
+            print("moved scrollV =", self.scrollbarV.get()[1])
+
+    def func_on_button1_cliecked(self):
+        tk.messagebox.showinfo("info", "[DEBUG] scrollbarV move_value=" + str(self.scrollbarV.get()[1]))
+
 
 if __name__ == "__main__":
     root = tk.Tk()
